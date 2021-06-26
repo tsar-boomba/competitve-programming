de6eac3c8867776e4c9f7c61f59eca822d627d37
tsar-boomba <itg.2048@gmail.com>
2021-06-26 18:04:12 -0400

    work on next round

--- Next-Round.cpp
+++ Next-Round.cpp
@@ -7,11 +7,27 @@
 using namespace std;
 
 int main() {
-    int n;
-    int k;
+    int n, k, a, m;
     cin >> n >> k;
-    while (n--) {
-        int a;
-        cin >> a; 
+    if ( n == k) {
+        cout << n;
+        return 0;
     }
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (i == 0) {
+            if (a < 1) {
+                cout << 0;
+                return 0;
+            }
+        }
+        if (i == k-1) {
+            m = a;
+        }
+        if (a < m) {
+            cout << i;
+            return 0;
+        }
+    }
+    cout << n;
 }
