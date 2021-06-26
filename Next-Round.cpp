e2008508347fe36d69073c27f53c4693bdcfc66f
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-06-26 09:42:33 -0400

    need to work on laptop :(

---
+++ Next-Round.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <cassert>
+
+using namespace std;
+
+int main() {
+    int n;
+    int k;
+    cin >> n >> k;
+    while (n--) {
+        int a;
+        cin >> a; 
+    }
+}
