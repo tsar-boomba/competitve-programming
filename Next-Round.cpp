d122939259597518b7a8c2cc76ccd103c9ca29b1
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-06-26 20:32:09 -0400

    scuffed next round answer :pensive:

--- Next-Round.cpp
+++ Next-Round.cpp
@@ -7,24 +7,24 @@
 using namespace std;
 
 int main() {
-    int n, k, a, m;
+    int n, k, a[100], m = 0;
     cin >> n >> k;
-    if ( n == k) {
-        cout << n;
-        return 0;
-    }
     for (int i = 0; i < n; i++) {
-        cin >> a;
+        cin >> a[i];
         if (i == 0) {
-            if (a < 1) {
+            if (a[i] < 1) {
                 cout << 0;
                 return 0;
             }
         }
-        if (i == k-1) {
-            m = a;
+        if (a[i] == 0) {
+            cout << i;
+            return 0;
         }
-        if (a < m) {
+        if (i == k-1) {
+            m = a[i];
+        }
+        if (a[i] < m) {
             cout << i;
             return 0;
         }
